d8cfbf8ae01dc106290f652decef27431916782e
Alyssa <alyssa.brooksong@gmail.com>
2023-07-28 15:09:13 -0700

    initialization

--- lib/audition.py
+++ lib/audition.py
@@ -1,2 +1,15 @@
 class Audition:
+
+    all = []
+
+    def __init__(self, role, actor, location, phone):
+        self.role = role
+        self.actor = actor
+        self.location = location
+        self.phone = phone
+        Audition.all.append(self)
+
+    def hired(self):
+        'true' if False else 'false'
+
     pass
--- lib/role.py
+++ lib/role.py
@@ -2,4 +2,24 @@ from lib.audition import Audition
 
 
 class Role:
-    pass
+
+    all = []
+
+    def __init__(self, character_name):
+        self.character_name = character_name
+        Role.all.append(self)
+
+    def get_character_name(self):
+        return self.character_name
+    
+    def set_character_name(self, character_name):
+        self.character_name = character_name
+        
+    def character_name(self):
+        if type(character_name) == str:
+            return character_name
+        else:
+            raise Exception
+
+
+Role("Hamlet")
